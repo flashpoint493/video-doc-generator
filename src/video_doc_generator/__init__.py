c3cc82290be29f9a274c1c4100394b5dfba538b0
Auto Project <ocarina1024@gmail.com>
2025-12-31 16:15:26 +0800

    chore: bump version to 0.1.2

--- src/video_doc_generator/__init__.py
+++ src/video_doc_generator/__init__.py
@@ -4,7 +4,7 @@ Video Doc Generator - 视频转文档生成器
 一个能够管理视频链接，调用视频解析 API，并将视频内容转换为专业文档的 Python 包。
 """
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from video_doc_generator.config import Config
 from video_doc_generator.generator import DocumentGenerator
